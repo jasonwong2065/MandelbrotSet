514d6fb611835b210d77560be0a45d044d80a93b
JordanWang123 <wcx.jordan@gmail.com>
2016-04-22 15:56:24 +1000

    The finished

---
+++ pixelColor.c
@@ -0,0 +1,25 @@
+
+#include "pixelColor.h"
+
+
+unsigned char stepsToRed (int steps){
+	unsigned char red;
+
+       red = steps - 1;
+
+	return red;
+}
+unsigned char stepsToBlue (int steps){
+	unsigned char blue;
+
+	   blue = steps - 1;
+
+        return blue;
+}
+unsigned char stepsToGreen (int steps){
+    unsigned char green;
+
+		green = steps - 1;
+
+	return green;
+}
